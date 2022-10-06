93877b27f83e812b2d02f3286f4d01f96208545f
Michael Nikishov <doopath@gmail.com>
2022-10-06 23:33:04 +0300

    static decorator Cache._check_if_opened has been refactored to a function decorator

--- modules/cache.py
+++ modules/cache.py
@@ -9,6 +9,16 @@ from modules.constants import APP_DIR
 from modules.exceptions import CacheSessionIsNotStartedException
 
 
+def check_if_opened(f: Callable) -> Callable:
+    def inner(*args, **kwargs) -> Any:
+        if args[0].is_opened:
+            return f(*args, **kwargs)
+        else:
+            raise CacheSessionIsNotStartedException("You didn't start the cache session!")
+
+    return inner
+
+
 class Cache:
     def __init__(self):
         self._cache_dir = APP_DIR
@@ -25,16 +35,6 @@ class Cache:
 
         return f"{self._cache_dir}/{self._cache_file}"
 
-    @staticmethod
-    def _check_if_opened(f: Callable) -> Callable:
-        def inner(*args, **kwargs) -> Any:
-            if args[0].is_opened:
-                return f(*args, **kwargs)
-            else:
-                raise CacheSessionIsNotStartedException("You didn't start the cache session!")
-
-        return inner
-
     def _initialize_cache(self) -> None:
         if not os.path.exists(self.cache_file_path):
             with open(self.cache_file_path, 'w') as cache_file:
@@ -42,32 +42,32 @@ class Cache:
 
         self.is_opened = True
 
-    @_check_if_opened
+    @check_if_opened
     def _get_cache(self) -> object:
         with open(self.cache_file_path, 'r') as cache_file:
             return json.load(cache_file)
 
-    @_check_if_opened
+    @check_if_opened
     def _initialize_local_cache(self) -> None:
         self._local_cache = self._get_cache()
 
-    @_check_if_opened
+    @check_if_opened
     def save_session(self) -> None:
         with open(self.cache_file_path, 'w') as cache_file:
             json.dump(self._local_cache, cache_file)
 
-    @_check_if_opened
+    @check_if_opened
     def close(self) -> None:
         self.is_opened = False
 
     def open(self) -> None:
         self.is_opened = True
 
-    @_check_if_opened
+    @check_if_opened
     def get_value(self, key: str) -> Any:
         return self._local_cache[key]
 
-    @_check_if_opened
+    @check_if_opened
     def get_couples(self, keys: list[Any]) -> dict[Any, Any]:
         res = {}
 
@@ -76,11 +76,11 @@ class Cache:
 
         return res
 
-    @_check_if_opened
+    @check_if_opened
     def set_value(self, key: Any, value: Any) -> None:
         self._local_cache[key] = value
 
-    @_check_if_opened
+    @check_if_opened
     def set_values(self, couples: dict[Any, Any]) -> None:
         for key in couples.keys():
             self._local_cache[key] = couples[key]
